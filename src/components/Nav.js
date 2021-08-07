2f0f64ee1fa64b221fafacd80699a329fe2f2c43
MGlower <danilkazaharovich@gmail.com>
2021-08-07 21:07:22 +0300

    first nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import Pin from '../images/pin_icon.svg';
+import Logo from '../images/logo.svg';
 
 export default function Nav() {
+  function openExtraList(e) {
+    e.preventDefault();
+    document.querySelectorAll('.nav__menu__item > a')
+    .forEach(el => {
+      el.classList.remove('active');
+      if(e.target === el) e.target.classList.add('active')
+    });
+  }
+
   return (
-    <ul>
-      <li>
-        <Link to="/products">Продукты</Link>
-      </li>
-      <li>
-        <Link to="/application">Приложение</Link>
-      </li>
-      <li>
-        <Link to="/distribution">Дистрибуция</Link>
-      </li>
-      <li>
-        <Link to="/where-to-buy">Где купить?</Link>
-      </li>
-    </ul>
+    <nav className="nav">
+      <div className="nav__logo__pin">
+        <img src={Pin} alt="" />
+      </div>
+      <NavLink to="/" className="nav__logo">
+        <img src={Logo} alt="" />
+      </NavLink>
+      <ul className='nav__menu'>
+        <li className='nav__menu__item'>
+          <NavLink to="/products">Продукты</NavLink>
+        </li>
+        <li className='nav__menu__item'>
+          <NavLink to="/application">Приложение</NavLink>
+        </li>
+        <li className='nav__menu__item'>
+          <a href='/' onClick={openExtraList}>Дистрибуция</a>
+        </li>
+        <li className='nav__menu__item'>
+          <NavLink to="/where-to-buy">Где купить?</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
